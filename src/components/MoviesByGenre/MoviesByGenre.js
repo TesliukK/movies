1acb0f767d6d450406f0dec35da70c607e7a2fe9
TesliukK <teslukkirill@gmail.com>
2023-02-24 00:07:22 +0200

    css

--- src/components/MoviesByGenre/MoviesByGenre.js
+++ src/components/MoviesByGenre/MoviesByGenre.js
@@ -1,14 +1,13 @@
-
 import {useSelector} from "react-redux";
 
 import {MoviesListCard} from "../MoviesListCard/MoviesListCard";
-
+import css from'./moviesByGenre.module.css';
 
 const MovieByGenre = () => {
     const {movieByGenre} = useSelector(state => state.genres);
     return (
-        <div>
-            {movieByGenre && movieByGenre.map(movie => <MoviesListCard key={movie.id} movie={movie}/>)}
+        <div className={css.block}>
+            {movieByGenre && movieByGenre.map(movie =><MoviesListCard key={movie.id} movie={movie}/>)}
         </div>
     );
 };
