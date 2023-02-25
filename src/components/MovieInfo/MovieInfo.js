ecbb90abfa998399a5d77862ee49a0845e383900
TesliukK <teslukkirill@gmail.com>
2023-02-25 20:16:01 +0200

    theme

--- src/components/MovieInfo/MovieInfo.js
+++ src/components/MovieInfo/MovieInfo.js
@@ -1,41 +1,50 @@
 import React from "react";
 import {useSelector} from "react-redux";
 
-
 import css from './movieInfo.module.css';
-
+import {Box, Rating} from "@mui/material";
 
 const MovieInfo = () => {
-
-    const {selectedMovie} = useSelector((state) => state.movies);
+    const { selectedMovie } = useSelector((state) => state.movies);
 
     return (
         <div className={css.container}>
             {selectedMovie && (
                 <div>
-                    <div><h1>{selectedMovie.original_title}</h1></div>
+                    <div>
+                        <h1>{selectedMovie.original_title}</h1>
+                    </div>
                     <div className={css.mediumBlock}>
-                        <div >
+                        <div>
                             <img
                                 src={`https://image.tmdb.org/t/p/w300${selectedMovie.poster_path}`}
                                 alt={selectedMovie.title}
                             />
                         </div>
+
                         <div>
                             <p>original_title: {selectedMovie.original_title}</p>
                             <p>popularity: {selectedMovie.popularity}</p>
                             <p>release_date: {selectedMovie.release_date}</p>
-
+                            <Box sx={{ display: 'flex', alignItems: 'center' }}>
+                                <Rating
+                                    name="movie-rating"
+                                    value={selectedMovie.vote_average / 2} // потрібно ділити на 2, оскільки значення vote_average знаходиться в діапазоні від 0 до 10, а Rating очікує значення від 0 до 5
+                                    precision={0.5}
+                                    emptyIcon={<MovieInfo style={{ opacity: 0.55 }} fontSize="inherit" />}
+                                />
+                                <Box sx={{ ml: 2 }}>{selectedMovie.vote_average}/10</Box>
+                            </Box>
                         </div>
                     </div>
                     <div>
                         <p>{selectedMovie.overview}</p>
                     </div>
-
                 </div>
             )}
         </div>
     );
 };
 
-export {MovieInfo};
+export { MovieInfo };
+
--- src/components/MoviesList/MoviesList.js
+++ src/components/MoviesList/MoviesList.js
@@ -6,6 +6,7 @@ import {useSearchParams} from "react-router-dom";
 import {movieAction} from "../../redux";
 import {MoviesListCard} from "../MoviesListCard/MoviesListCard";
 import css from './movieList.module.css';
+import {Pagination} from "@mui/material";
 
 
 const MoviesList = () => {
@@ -25,13 +26,17 @@ const MoviesList = () => {
                 {movies.map(movie => <MoviesListCard key={movie.id} movie={movie}/>)}
             </div>
             <div className={css.btnBlock}>
-                <button className={css.btn} disabled={page <= 1}
-                        onClick={() => setQuery(query => ({page: +query.get('page') - 1}))}>prev
-                </button>
-                <div>----{page}----</div>
-                <button className={css.btn} disabled={page >= 500}
-                        onClick={() => setQuery(query => ({page: +query.get('page') + 1}))}>next
-                </button>
+                <Pagination
+                    count={500}
+                    onChange={(event, value) => setQuery(query => ({ ...query, page: value }))}
+                    disabled={page <= 0 || page >= 501}
+                    color="primary"
+                    shape="rounded"
+                    size="large"
+                    showFirstButton
+                    showLastButton
+                />
+
             </div>
         </div>
 
